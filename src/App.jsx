d95fa1261594a4c4d8dd3f085f6dd535511a1303
Wallyson <wallysom2@gmail.com>
2022-04-03 06:18:52 -0400

    construindo pag de registro

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Habits from "./components/Habits";
+import Today from "./components/Today";
+import Historic from "./components/Historic";
+import Topbar from "./components/Topbar";
+import Menu from "./components/Menu";
+import Day from "./components/Day";
+import UserContext from "./contexts/UserContext";
+import TokenContext from "./contexts/TokenContext";
+import PercentageContext from "./contexts/PercentageContext";
+import DayContext from "./contexts/DayContext"
 import "./style/reset.css";
 import "./style/style.css";
 
@@ -40,6 +50,10 @@ export default function App() {
                 <Routes>
                   <Route path="/" element={<Login />} />
                   <Route path="/register" element={<Register />} />
+                  <Route path="/habits" element={<Habits />} />
+                  <Route path="/today" element={<Today />} />
+                  <Route path="/historic" element={<Historic />} />
+                  <Route path="/Day/:idHabit" element={<Day />} />
                 </Routes>
               <Menu pathname={window.location.pathname}/>
             </BrowserRouter>
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
-import App from "./App.js";
+import App from "./App.jsx";
 
 const app = <App />;
 const root = document.querySelector(".root");
